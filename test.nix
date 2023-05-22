a0b7c22f77c0a08cfdf2c204a3a64a2f1f8a943e
Guillaume Girol <symphorien+git@xlumurb.eu>
2023-05-22 12:00:00 +0000

    test that the project still builds on current stable nixos release

---
+++ test.nix
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2023 Guillaume Girol <symphorien+git@xlumurb.eu>
+#
+# SPDX-License-Identifier: CC0-1.0
+
+map (channel: let
+  pkgs = import (builtins.fetchTarball ("channel:"+channel)) {};
+  nixseparatedebuginfod = pkgs.callPackage ./nixseparatedebuginfod.nix {};
+in
+  nixseparatedebuginfod.overrideAttrs ({name, ...}: {
+    name = name + "-" + channel;
+  })
+  ) [ "nixos-unstable" "nixos-22.11" ]
+
