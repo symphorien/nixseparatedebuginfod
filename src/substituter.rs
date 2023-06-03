b883160368150d4835fca8b26049cf4dd40871ca
Guillaume Girol <symphorien+git@xlumurb.eu>
2023-06-03 12:00:00 +0000

    wip async

--- src/substituter.rs
+++ src/substituter.rs
@@ -9,6 +9,7 @@ use std::{
 };
 
 use anyhow::Context;
+use async_trait::async_trait;
 use serde::Deserialize;
 use tempfile::TempDir;
 
@@ -24,7 +25,7 @@ struct DebuginfoMetadata {
 }
 
 /// Returns the 32 first bytes of the specified file
-fn magic(path: &Path) -> anyhow::Result<Vec<u8>> {
+async fn magic(path: &Path) -> anyhow::Result<Vec<u8>> {
     let mut res = vec![b'\0'; 32];
     let mut file = std::fs::File::open(path)
         .with_context(|| format!("reading magic of {}", path.display()))?;
@@ -37,47 +38,50 @@ const NAR_MAGIC: &'static [u8] = b"\x0d\x00\x00\x00\x00\x00\x00\x00nix-archive-1
 const ELF_MAGIC: &'static [u8] = b"\x7fELF";
 
 /// API to fetch debuginfo indices from substituters
+#[async_trait]
 pub trait Substituter: Sized {
     /// If this type can handle the scheme of this url,
     /// returns an instance, otherwise None
     /// cache_dir is a directory where requested debuginfo must be stored
-    fn from_url(url: &str) -> anyhow::Result<Option<Self>>;
+    async fn from_url(url: &str) -> anyhow::Result<Option<Self>>;
 
     /// Fetches a file from the substituter indexed by its relative path
     /// to the root
     ///
     /// Returns None in case of missing file.
-    fn fetch(&self, path: &Path) -> anyhow::Result<Option<PathBuf>>;
+    async fn fetch(&self, path: &Path) -> anyhow::Result<Option<PathBuf>>;
 
     /// the url used to construct this substituter
     fn url(&self) -> &str;
 
     /// returns a store path containing the requested debuginfo in
     /// `/lib/debug/.build-id`
-    fn fetch_debuginfo(&self, buildid: &str) -> anyhow::Result<Option<PathBuf>> {
+    async fn fetch_debuginfo(&self, buildid: &str) -> anyhow::Result<Option<PathBuf>> {
         let path = PathBuf::from(format!("debuginfo/{buildid}.debug"));
-        self.fetch_debuginfo_from(path.as_path(), 2)
+        self.fetch_debuginfo_from(path.as_path(), 2).await
     }
 
     /// attempt to fetch debuginfo in this relative path inside the substituter
     ///
     /// returns a store path containing it
-    fn fetch_debuginfo_from(
+    async fn fetch_debuginfo_from(
         &self,
         path: &Path,
         max_redirects: usize,
     ) -> anyhow::Result<Option<PathBuf>> {
         let file = self
             .fetch(path)
+            .await
             .with_context(|| format!("fetching {} from {}", path.display(), self.url()))?;
         let file = match file {
             None => return Ok(None),
             Some(f) => f,
         };
-        let tempfile;
+        let temppath;
         let target;
-        let dir_to_add = match &magic(file.as_path())? {
+        let dir_to_add = match &magic(file.as_path()).await? {
             m if m.starts_with(ELF_MAGIC) => {
+                // sync code
                 let buildid = match get_buildid(file.as_path()).with_context(|| {
                     format!(
                         "buildid of elf file fetched from {} in {}",
@@ -96,10 +100,12 @@ pub trait Substituter: Sized {
                 target = dir.path().join("target-nar");
                 let mut parent = target.join("lib/debug/.build-id");
                 parent.push(&buildid[..2]);
-                std::fs::create_dir_all(parent.as_path())
+                tokio::fs::create_dir_all(parent.as_path())
+                    .await
                     .with_context(|| format!("creating {}", parent.display()))?;
                 parent.push(format!("{}.debug", &buildid[2..]));
-                std::fs::copy(file.as_path(), parent.as_path())
+                tokio::fs::copy(file.as_path(), parent.as_path())
+                    .await
                     .context("copying debuginfo file")?;
                 target.as_path()
             }
@@ -109,6 +115,8 @@ pub trait Substituter: Sized {
                     anyhow::bail!("too many redirects");
                 }
                 // this is a json redirect
+                //
+                // sync code
                 let file = std::fs::File::open(file.as_path()).with_context(|| {
                     format!("opening {} to deserialize as json", path.display())
                 })?;
@@ -127,37 +135,47 @@ pub trait Substituter: Sized {
                     self.url(),
                     &metadata.archive
                 );
-                return self.fetch_debuginfo_from(redirect_path.as_path(), max_redirects - 1);
+                return self
+                    .fetch_debuginfo_from(redirect_path.as_path(), max_redirects - 1)
+                    .await;
             }
             m => {
                 let nar_file = if m.starts_with(NAR_MAGIC) {
                     file.as_path() // a nar file
                 } else {
                     // this is a compressed nar probably
-                    tempfile = tempfile::NamedTempFile::new().context("tempfile")?;
-                    let fd = std::fs::File::open(&file).context("unxz")?;
-                    compress_tools::uncompress_data(fd, &tempfile).with_context(|| {
-                        format!("unpacking {} from {}", file.display(), self.url())
-                    })?;
-                    if magic(tempfile.path())
+                    temppath = tempfile::NamedTempFile::new()
+                        .context("temppath")?
+                        .into_temp_path();
+                    let out = tokio::fs::File::create(&temppath)
+                        .await
+                        .context("opening temppath")?;
+                    let fd = tokio::fs::File::open(&file).await.context("unxz")?;
+                    compress_tools::tokio_support::uncompress_data(fd, out)
+                        .await
+                        .with_context(|| {
+                            format!("unpacking {} from {}", file.display(), self.url())
+                        })?;
+                    if magic(temppath.as_ref())
+                        .await
                         .context("magic of uncompressed nar")?
                         .starts_with(NAR_MAGIC)
                     {
-                        tempfile.path()
+                        temppath.as_ref()
                     } else {
                         anyhow::bail!("nar {} was not a compressed nar", path.display());
                     }
                 };
                 // unpack the nar
-                let fd = std::fs::File::open(nar_file)?;
-                let mut cmd = std::process::Command::new("nix-store");
+                let fd = tokio::fs::File::open(nar_file).await?;
+                let mut cmd = tokio::process::Command::new("nix-store");
                 cmd.arg("--restore");
                 let dir = tempfile::TempDir::new().context("tempdir")?;
                 // FIXME: the indexer should probably not take the name of the store path into account
                 target = dir.as_ref().join("nar-debug");
                 cmd.arg(target.as_path());
-                cmd.stdin(fd);
-                let status = cmd.status().with_context(|| {
+                cmd.stdin(fd.into_std().await);
+                let status = cmd.status().await.with_context(|| {
                     format!(
                         "running nix-store --import to unpack nar from {} in {}",
                         nar_file.display(),
@@ -176,10 +194,10 @@ pub trait Substituter: Sized {
         };
 
         // add it to the store
-        let mut cmd = std::process::Command::new("nix-store");
+        let mut cmd = tokio::process::Command::new("nix-store");
         cmd.arg("--add");
         cmd.arg(dir_to_add);
-        let output = cmd.output().context("nix-store --add")?;
+        let output = cmd.output().await.context("nix-store --add")?;
         anyhow::ensure!(
             output.status.success(),
             "nix-store --add failed: {:?}: {}",
@@ -213,8 +231,9 @@ pub struct FileSubstituter {
     url: String,
 }
 
+#[async_trait]
 impl Substituter for FileSubstituter {
-    fn from_url(url: &str) -> anyhow::Result<Option<Self>> {
+    async fn from_url(url: &str) -> anyhow::Result<Option<Self>> {
         if !url.starts_with("file:///") {
             return Ok(None);
         }
@@ -233,7 +252,7 @@ impl Substituter for FileSubstituter {
         }))
     }
 
-    fn fetch(&self, path: &Path) -> anyhow::Result<Option<PathBuf>> {
+    async fn fetch(&self, path: &Path) -> anyhow::Result<Option<PathBuf>> {
         anyhow::ensure!(
             path.is_relative(),
             "substituter path {} should be relative",
@@ -252,30 +271,32 @@ impl Substituter for FileSubstituter {
     }
 }
 
-#[test]
-fn file_substituter_from_url() {
+#[tokio::test]
+async fn file_substituter_from_url() {
     let d = TempDir::new().unwrap();
     assert!(matches!(
-        FileSubstituter::from_url("https://cache.nixos.rg"),
+        FileSubstituter::from_url("https://cache.nixos.rg").await,
         Ok(None)
     ));
     assert!(matches!(
-        FileSubstituter::from_url(&format!("file://{}/doesnotexist", d.path().display())),
+        FileSubstituter::from_url(&format!("file://{}/doesnotexist", d.path().display())).await,
         Err(_)
     ));
     let ok = FileSubstituter::from_url(&format!("file://{}/./", d.path().display()))
+        .await
         .unwrap()
         .unwrap();
     assert_eq!(&ok.path, d.path());
 }
 
-#[test]
-fn file_substituter_fetch() {
+#[tokio::test]
+async fn file_substituter_fetch() {
     let d = TempDir::new().unwrap();
     let ok = FileSubstituter::from_url(&format!("file://{}/./", d.path().display()))
+        .await
         .unwrap()
         .unwrap();
     let path = d.path().join("file");
     std::fs::write(&path, "yay").unwrap();
-    assert_eq!(ok.fetch(Path::new("./file")).unwrap().unwrap(), path);
+    assert_eq!(ok.fetch(Path::new("./file")).await.unwrap().unwrap(), path);
 }
